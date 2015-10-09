eb13215b42644ab18fdef4d101aa54074b0e4af1
dare0021 <dare0021@kaist.ac.kr>
2015-10-09 12:06:37 +0900

    Forgot to do git add before commit. Submarine and Torpedo classes gone. Yay. Added Submarine stub so code now compiles

---
+++ Classes/Entities/Submarine.cpp
@@ -0,0 +1,8 @@
+﻿#include "Submarine.h"
+#include "ui/CocosGUI.h"
+
+USING_NS_CC;
+
+Submarine::Submarine(std::string path) : O3Sprite(path)
+{
+}
---
+++ Classes/Entities/Submarine.h
@@ -0,0 +1,13 @@
+#ifndef __SUBMARINE_H__
+#define __SUBMARINE_H__
+
+#include "cocos2d.h"
+#include "O3Sprite.h"
+
+class Submarine : public O3Sprite
+{
+public:
+	Submarine(std::string path);
+};
+
+#endif // __SUBMARINE_H__
